857d27581ccac071614089fc8de3c8fdb199bf40
nadesen <mononokehimehafukai@gmail.com>
2025-11-07 05:06:35 +0000

    Bootstrapd導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  # 新規登録後遷移先
+  def after_sign_up_path_for(resource)
+    root_path
+  end
+
+  # ログイン後遷移先
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
+  # ログアウト後遷移先
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :nickname])
+  end
 end
--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,2 +1,9 @@
 class Public::UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+    @quests = @user.quests.page(params[:page]).per(6)
+  end
+
+  
 end
