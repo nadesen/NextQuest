5b612970a4a28357c75d9e6e6595730c63683a1f
nadesen <mononokehimehafukai@gmail.com>
2026-01-26 11:37:01 +0000

    review_comments_controllerリファクタリング

--- app/controllers/public/review_comments_controller.rb
+++ app/controllers/public/review_comments_controller.rb
@@ -1,23 +1,17 @@
-class  Public::ReviewCommentsController < ApplicationController
+class Public::ReviewCommentsController < ApplicationController
   before_action :authenticate_user!
   before_action :forbid_guest_user!, only: [:create, :destroy]
 
+  # POST /reviews/:review_id/review_comments
   def create
-    @review = Review.find(params[:review_id])
-    # 管理者以外で未承認レビューはコメント禁止
-    unless @review.approved? || (current_user.respond_to?(:admin?) && current_user.admin?)
-      respond_to do |format|
-        format.html { redirect_to review_path(@review), alert: "承認されていないレビューにはコメントできません。" }
-        format.js { render js: "alert('承認されていないレビューにはコメントできません。');", status: :forbidden }
-      end
-      return
-    end
-  
+    @review = find_and_authorize_review!
+    return if performed?
+
     @comment = current_user.review_comments.new(review_comment_params)
     @comment.review = @review
     @comment.score = Language.get_data(@comment.comment)
-    @review_comments = @review.review_comments.order(created_at: :asc)
-  
+    @review_comments = ordered_comments(@review)
+
     if @comment.save
       respond_to do |format|
         format.js
@@ -34,21 +28,52 @@ class  Public::ReviewCommentsController < ApplicationController
     end
   end
 
+  # DELETE /review_comments/:id
   def destroy
     @comment = ReviewComment.find(params[:id])
     @review = @comment.review
-    if @comment.user == current_user
+    if owns_comment?(@comment)
       @comment.destroy
+    else
+      # 本来ここでリダイレクト等しても良い
+      flash[:alert] = '削除権限がありません。'
     end
-    @review_comments = @review.review_comments.order(created_at: :asc)
-
+    @review_comments = ordered_comments(@review)
     respond_to do |format|
-      format.js   # destroy.js.erb を返す
+      format.js
       format.html { redirect_to review_path(@review) }
     end
   end
 
   private
+
+  # レビュー取得＋未承認なら管理者以外は禁止
+  def find_and_authorize_review!
+    review = Review.find(params[:review_id])
+    unless review.approved? || admin_user?
+      respond_to do |format|
+        format.html { redirect_to review_path(review), alert: "承認されていないレビューにはコメントできません。" }
+        format.js   { render js: "alert('承認されていないレビューにはコメントできません。');", status: :forbidden }
+      end
+      return nil # フィルター内 return
+    end
+    review
+  end
+
+  # コメントリスト取得（並び順も共通化）
+  def ordered_comments(review)
+    review.review_comments.order(created_at: :asc)
+  end
+
+  # 管理者ユーザーか判定
+  def admin_user?
+    current_user.respond_to?(:admin?) && current_user.admin?
+  end
+
+  # コメントの所有者か？
+  def owns_comment?(comment)
+    comment.user == current_user
+  end
 
   def review_comment_params
     params.require(:review_comment).permit(:comment)
